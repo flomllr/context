936c7556729170eeea2725efff5ae9819489e625
Berzan Yildiz <berzan.yildiz@tum.de>
2019-09-28 18:49:51 +0200

    Opens new desktop to open context

--- callscript.js
+++ callscript.js
@@ -128,8 +128,27 @@ const openSublimeFile = (path) => new Promise((resolve, reject) => {
     }
 });
 
+const openDesktop = async (addButton) => 
+    await tryRunApplescript(`
+    tell application "System Events"
+	tell application "Mission Control" to launch
+	tell group 2 of group 1 of group 1 of process "Dock"
+		click (every button whose value of attribute "AXDescription" is "${addButton}")
+		tell list 1
+			set countSpaces to count of buttons
+			delay 0.5
+			click button (countSpaces)
+		end tell
+	end tell
+end tell
+    `);
+
 (async () => {
+    console.log("Saving")
     await saveCurrentProfileAs("test")
+    console.log("Desktop switching")
+    await openDesktop("añadir escritorio")
+    console.log("Opening profile")
     await openProfile("test")
 })();
 
